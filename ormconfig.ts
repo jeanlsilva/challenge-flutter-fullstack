101658d1f4f1bccb372bac66535a9883d5be31e4
Jean Silva <jeanslucasilva@gmail.com>
2022-06-03 01:12:29 -0300

    ormconfig adjustments

---
+++ ormconfig.ts
@@ -0,0 +1,17 @@
+import { ConnectionOptions } from 'typeorm';
+
+const config: ConnectionOptions = {
+  type: 'postgres',
+  host: process.env.PG_HOST,
+  port: Number(process.env.PG_PORT),
+  username: process.env.PG_USER,
+  password: process.env.PG_PASSWORD,
+  database: process.env.PG_DATABASE,
+  entities: ['./src/models/*.ts'],
+  migrations: ['./src/database/migrations/*.ts'],
+  cli: {
+    migrationsDir: './src/database/migrations ',
+  },
+};
+
+module.exports = config;
