96422fd2f00e8926da1caee81b71ca3e7a418c1f
Jean Silva <jeanslucasilva@gmail.com>
2022-06-03 12:17:07 -0300

    ormconfig adjustments

--- ormconfig.ts
+++ ormconfig.ts
@@ -3,11 +3,11 @@ console.log('process.env.DATABASE.URL :>> ', process.env.DATABASE_URL);
 const config = {
   type: 'postgres',
   url: process.env.DATABASE_URL,
-  entities: ['./dist/models/**/*.js'],
-  migrations: ['./dist/database/migrations/**/*.js'],
+  entities: ['/dist/models/**/*.js'],
+  migrations: ['/dist/database/migrations/**/*.js'],
   cli: {
-    migrationsDir: './src/database/migrations ',
-    entitiesDir: './src/models',
+    migrationsDir: '/src/database/migrations ',
+    entitiesDir: '/src/models',
   },
 };
 
