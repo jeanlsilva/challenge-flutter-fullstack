649a98146eef0e660e22c86395011e8dc09093e4
Jean Silva <jeanslucasilva@gmail.com>
2022-06-06 08:18:31 -0300

    type module

---
+++ dist/config/auth.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.default = {
+    jwt: {
+        secret: '2GAhYNhzGtCAMhXE3Fzy203GeWQG6CjAeveR4K3MRG9oPpL9Zn9H9HHngtK9iXq',
+        expiresIn: '1d',
+    },
+};
